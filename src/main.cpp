6b221463b36d6b9fd6abe2859afcf458cc68fa1a
ModischFabrications <modisch.fabrications@gmail.com>
2019-12-10 20:41:24 +0100

    add serial heartbeat

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
-#include <Arduino.h>
+//#include <Arduino.h>
+#include <ESP8266WiFi.h>    // we have WiFi
+#include <WiFiManager.h>    // captive portal
+
+//#include <HardwareSerial.h>
+
+const bool USE_SERIAL = true;
+
+// could (should?) be moved into other files
+const uint8_t PIN_RGB = D1;
+const uint8_t N_LEDS = 24;
+
+void serial_setup() {
+    if (!USE_SERIAL) return;
+
+    Serial.begin(9600);
+    Serial.println();
+    Serial.println(F("Welcome to Verdandi"));
+    Serial.println(F("Debug mode is activated"));
+}
+
+void serial_heartbeat() {
+    if (!USE_SERIAL) return;
+    static uint32_t last_msg = 0;
+    const uint16_t time_interval = 5 * 1000;
+
+    uint32_t time = millis();
+
+    if (time - last_msg > time_interval) {
+        Serial.println(F("."));
+        last_msg = time;
+    }
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
+    serial_setup();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    delay(100);
+
+    serial_heartbeat();
 }
+
+
