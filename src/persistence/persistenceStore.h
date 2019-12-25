e3f57cfaf6228c45a2abff84b5afa4f44688a86a
ModischFabrications <modisch.fabrications@gmail.com>
2019-12-25 23:20:32 +0100

    add wear leveling

--- src/persistence/persistenceStore.h
+++ src/persistence/persistenceStore.h
@@ -1,5 +1,6 @@
 #pragma once
 
+//https://github.com/esp8266/Arduino/tree/master/libraries/EEPROM
 #include <EEPROM.h>
 
 #include "configuration.h"
@@ -7,29 +8,32 @@
 // TODO: Preserve EEPROM while programming -> EESAVE fuse
 // TODO: address = f(version, sizeof(Configuration))
 
+/* change with each design iteration to prevent EEPROM inconsistency and help
+ * with wear leveling of EEPROM cells.
+ * The chance that a random, unset combination is a match is very low.
+ */
+const uint8_t VERSION = 2;
+
 /**
  * EEPROM has around 100k writes per cell, so use them carefully!
  * It might be a good idea to switch to new cells with each revision.
  * ATtiny has 512 bytes EEPROM.
  *
- * I think wear leveling is logically impossible to automate without external
- * input. Storage pointers burn out the pointer cell, random won't be found
+ * Wwear leveling is logically impossible to automate without external
+ * input, e.g. version numbers.
+ * Storage pointers burn out the pointer cell, random won't be found
  * after a restart.
  * */
-const uint16_t EEPROM_VERSION_ADDR = 0;
-const uint16_t EEPROM_SETTINGS_ADDR = EEPROM_VERSION_ADDR + 1;
+const uint16_t EEPROM_VERSION_ADDR = VERSION;
+const uint16_t EEPROM_SETTINGS_ADDR = EEPROM_VERSION_ADDR + sizeof(VERSION);
 
-// change with each design iteration to prevent EEPROM inconsistency
-// the chance that a random, unset combination is a match is very low
-const uint8_t version = 2;
-
-const uint16_t BYTES_USED = 512;
+const uint16_t MAX_BYTES_USED = 512;
 
 // -----------
 
 void setup_settings() {
     // ESP needs to know how much storage we actually need
-    EEPROM.begin(BYTES_USED);
+    EEPROM.begin(MAX_BYTES_USED);
 }
 
 /**
@@ -37,7 +41,7 @@ void setup_settings() {
  * TODO: check if convenience features actually use 2 bytes as before
  * */
 void save_settings(Configuration settings) {
-    EEPROM.write(EEPROM_VERSION_ADDR, version);
+    EEPROM.write(EEPROM_VERSION_ADDR, VERSION);
     EEPROM.put(EEPROM_SETTINGS_ADDR, settings);
 
     // commit planned changes
@@ -52,7 +56,7 @@ Configuration load_settings() {
     // could be rubbish or zeros, either way should work
     uint8_t saved_version = EEPROM.read(EEPROM_VERSION_ADDR);
 
-    if (saved_version != version) {
+    if (saved_version != VERSION) {
         // save new default settings, updates version
         save_settings(default_configuration);
 
