88c8e0a3fa4f2c34fd1d095b5f8cdfaa6f50c798
unknown <luk.popp0@gmail.com>
2019-12-30 21:14:29 +0100

    fixed some configuration issues

--- src/persistence/configuration.h
+++ src/persistence/configuration.h
@@ -16,9 +16,20 @@ struct Configuration {
     uint16_t pollInterval;
 
     const bool operator==(const Configuration& other) {
-        // FIXME: extend with correct comparison
         return (this->brightness == other.brightness &&
-                this->showMinutes == other.showMinutes);
+                this->showHours == other.showHours &&
+                this->showMinutes == other.showMinutes &&
+                this->showSeconds == other.showSeconds &&
+                this->colorH[0] == other.colorH[0] &&
+                this->colorH[1] == other.colorH[1] &&
+                this->colorH[2] == other.colorH[2] &&
+                this->colorM[0] == other.colorM[0] &&
+                this->colorM[1] == other.colorM[1] &&
+                this->colorM[2] == other.colorM[2] &&
+                this->colorS[0] == other.colorS[0] &&
+                this->colorS[1] == other.colorS[1] &&
+                this->colorS[2] == other.colorS[2] &&
+                this->pollInterval == other.pollInterval);
     }
 };
 
