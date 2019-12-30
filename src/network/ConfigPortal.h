59f404ce3a33cb5a54103f7795eee3dce93f7f37
unknown <luk.popp0@gmail.com>
2019-12-30 19:45:15 +0100

    added missing config argument parsing

--- src/network/ConfigPortal.h
+++ src/network/ConfigPortal.h
@@ -95,10 +95,49 @@ String ArgsToString() {
 Configuration ArgsToConfiguration() {
     Configuration new_values = Configuration();
 
-    // FIXME: implement argument parsing
     // implicit clamping could make problems but it's just nicer to read
-    if (server.argName(0) == "brightness") new_values.brightness = server.arg(0).toInt();
-
+    if (server.argName(0) == "brightness")
+        new_values.brightness = server.arg(0).toInt();
+    else println(F("Value 0 not found"));
+    if (server.argName(1) == "showHours")
+        new_values.showHours = server.arg(1) == "true" ? true : false;
+    else println(F("Value 1 not found"));
+    if (server.argName(2) == "showMinutes")
+        new_values.showMinutes = server.arg(2) == "true" ? true : false;
+    else println(F("Value 2 not found"));
+    if (server.argName(3) == "showSeconds")
+        new_values.showSeconds = server.arg(3) == "true" ? true : false;
+    else println(F("Value 3 not found"));
+    if (server.argName(4) == "colorH_R")
+        new_values.colorH[0] = server.arg(4).toInt();
+    else println(F("Value 4 not found"));
+    if (server.argName(5) == "colorH_G")
+        new_values.colorH[1] = server.arg(5).toInt();
+    else println(F("Value 5 not found"));
+    if (server.argName(6) == "colorH_B")
+        new_values.colorH[2] = server.arg(6).toInt();
+    else println(F("Value 6 not found"));
+    if (server.argName(7) == "colorM_R")
+        new_values.colorM[0] = server.arg(7).toInt();
+    else println(F("Value 7 not found"));
+    if (server.argName(8) == "colorM_G")
+        new_values.colorM[1] = server.arg(8).toInt();
+    else println(F("Value 8 not found"));
+    if (server.argName(9) == "colorM_B")
+        new_values.colorM[2] = server.arg(9).toInt();
+    else println(F("Value 9 not found"));
+    if (server.argName(10) == "colorS_R")
+        new_values.colorS[0] = server.arg(10).toInt();
+    else println(F("Value 10 not found"));
+    if (server.argName(11) == "colorS_G")
+        new_values.colorS[1] = server.arg(11).toInt();
+    else println(F("Value 11 not found"));
+    if (server.argName(12) == "colorS_B")
+        new_values.colorS[2] = server.arg(12).toInt();
+    else println(F("Value 12 not found"));
+    if (server.argName(13) == "pollInterval")
+        new_values.pollInterval = server.arg(13).toInt();
+    else println(F("Value 13 not found"));
 
     return new_values;
 }
