ae2d92cb761aab5f2f38c545ff60875ea56d1f08
unknown <luk.popp0@gmail.com>
2020-01-02 21:37:24 +0100

    updated time service 3

--- src/network/timeService.h
+++ src/network/timeService.h
@@ -1,30 +1,60 @@
 #pragma once
 
-#include <NTPClient.h>
+// #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <sntp.h>
+#include <sys/time.h>
+#include <time.h>
 
+#include "TZ.h"
 #include "serial/SerialWrapper.h"
 
 namespace TimeService {
+
+struct Time {
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    uint16_t millisecond;
+};
+
 namespace {
+const char* TIMEZONE = "TZ_Europe_London";
+
+timeval tv;
+time_t now;
+uint32_t now_ms;
+
+Time currentTime = {0, 0, 0, 0};
+
 // TODO: make parameter
 // utc offset for your timezone: e.g. GMT +1 = 3600
 // can be set with timeClient.setTimeOffset(offset);
 const long utcOffsetInSeconds = 3600;
+
+// WiFiUDP ntpUDP;
+// NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 } // namespace
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+void setup();
+void timeUpdate();
+Time getCurrentTime();
 
-void setup() { timeClient.begin(); }
+void setup() {
+    configTime(TIMEZONE, "pool.ntp.org");
+
+    // timeClient.begin();
+}
 
 void timeUpdate() {
-    while (!timeClient.update()) {
-        timeClient.forceUpdate();
-    }
-
-    String formattedTime = timeClient.getFormattedTime();
-    print(F("TIME: "));
-    printlnRaw(formattedTime);
+    gettimeofday(&tv, nullptr);
+    time_t now = time(nullptr);
+    tm* t = localtime(&now);
+    currentTime.hour = t->tm_hour;
+    currentTime.minute = t->tm_min;
+    currentTime.second = t->tm_sec;
+    currentTime.millisecond = millis() % 1000;
 }
+
+Time getCurrentTime() { return currentTime; }
 } // namespace TimeService
