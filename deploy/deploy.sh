6da26502d34373d5515a26cc655302021ab1285e
ModischFabrications <modisch.fabrications@gmail.com>
2020-01-28 23:21:13 +0100

    remove old TODO

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,6 @@ echo "[MPC] --- Deploying the whole project --- "
 
 command -v pio >/dev/null 2>&1 || { echo >&2 "pio can't be found. Add to PATH!"; exit 1; }
 
-# TODO: won't exit correctly
 echo "[MPC] Uploading file system"
 ./deploy/deploy_fs.sh || exit $?
 
