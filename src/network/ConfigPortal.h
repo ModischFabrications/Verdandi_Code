b0ff328d27ba4e6f90fac30cfce996b81aee474f
Lukas Popp <luk.popp0@gmail.com>
2020-02-24 18:52:31 +0100

    add error messages

--- src/network/ConfigPortal.h
+++ src/network/ConfigPortal.h
@@ -216,51 +216,51 @@ Config::Configuration ArgsToConfiguration() {
     if (server.argName(0) == "brightness")
         newValues.brightness = server.arg(0).toInt();
     else
-        println(F("Value 0 not found"));
+        logError(F("Value 0 not found"));
 
     if (server.argName(1) == "showHours")
         newValues.showHours = server.arg(1) == "true" ? true : false;
     else
-        println(F("Value 1 not found"));
+        logError(F("Value 1 not found"));
     if (server.argName(2) == "showMinutes")
         newValues.showMinutes = server.arg(2) == "true" ? true : false;
     else
-        println(F("Value 2 not found"));
+        logError(F("Value 2 not found"));
     if (server.argName(3) == "showSeconds")
         newValues.showSeconds = server.arg(3) == "true" ? true : false;
     else
-        println(F("Value 3 not found"));
+        logError(F("Value 3 not found"));
 
     if (server.argName(4) == "colorH") {
         const char* input = server.arg(4).c_str();
         splitCSVToArray(input, newValues.colorH);
         printArray(newValues.colorH, 3);
     } else
-        println(F("Value 4 not found"));
+        logError(F("Value 4 not found"));
 
     if (server.argName(5) == "colorM") {
         const char* input = server.arg(5).c_str();
         splitCSVToArray(input, newValues.colorM);
         printArray(newValues.colorM, 3);
     } else
-        println(F("Value 5 not found"));
+        logError(F("Value 5 not found"));
 
     if (server.argName(6) == "colorS") {
         const char* input = server.arg(6).c_str();
         splitCSVToArray(input, newValues.colorS);
         printArray(newValues.colorS, 3);
     } else
-        println(F("Value 6 not found"));
+        logError(F("Value 6 not found"));
 
     if (server.argName(7) == "pollInterval")
         newValues.pollInterval = max(server.arg(7).toInt(), (long)1);
     else
-        println(F("Value 7 not found"));
+        logError(F("Value 7 not found"));
 
     if (server.argName(8) == "nightmode")
         newValues.nightmode = server.arg(8) == "true" ? true : false;
     else
-        println(F("Value 8 not found"));
+        logError(F("Value 8 not found"));
     if (server.argName(9) == "turnOffAt") {
         newValues.turnOffAt = Time{(uint8_t)server.arg(9).toInt()};
         const char* input = server.arg(9).c_str();
@@ -269,7 +269,7 @@ Config::Configuration ArgsToConfiguration() {
         splitCSVToArray(input, times, ":");
         newValues.turnOffAt = Time{times[0], times[1]};
     } else
-        println(F("Value 9 not found"));
+        logError(F("Value 9 not found"));
     if (server.argName(10) == "turnOnAt") {
         newValues.turnOnAt = Time{(uint8_t)server.arg(10).toInt()};
         const char* input = server.arg(10).c_str();
@@ -277,20 +277,20 @@ Config::Configuration ArgsToConfiguration() {
         splitCSVToArray(input, times, ":");
         newValues.turnOnAt = Time{times[0], times[1]};
     } else
-        println(F("Value 10 not found"));
+        logError(F("Value 10 not found"));
 
     if (server.argName(11) == "timezone")
         strcpy(newValues.timezone, server.arg(11).c_str());
     else
-        println(F("Value 11 not found"));
+        logError(F("Value 11 not found"));
     if (server.argName(12) == "timezoneName")
         strcpy(newValues.timezoneName, server.arg(12).c_str());
     else
-        println(F("Value 12 not found"));
+        logError(F("Value 12 not found"));
     if (server.argName(13) == "useFading")
         newValues.useFading = server.arg(13) == "true" ? true : false;
     else
-        println(F("Value 13 not found"));
+        logError(F("Value 13 not found"));
 
 
     // add new fields here
